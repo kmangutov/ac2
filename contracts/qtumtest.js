cf61bd88ef65d08b9a695edacb0ac26ac3b3d309
Kent <khmakishima@ucdavis.edu>
2018-09-04 20:01:25 -0700

    updated test

--- contracts/qtumtest.js
+++ contracts/qtumtest.js
@@ -5,17 +5,17 @@ const rpc = new QtumRPC("")
 async function main() {
   const contractInfo = require("./insurance.json") //yet to be added
 
-  const foo = new Contract(rpc, contractInfo)
+  const test = new Contract(rpc, contractInfo)
 
   const i = 400 //arbitrary set insurance test value
   console.log("setInsurance", i)
-  const receipt = await foo.send("setInsurance", [i])
+  const receipt = await test.send("setInsurance", [i])
 
   console.log("txid", receipt.txid)
   console.log("waiting for transaction confirmation")
   await receipt.done(1)
 
-  const callResult = await foo.call("insurance_current")
+  const callResult = await test.call("insurance_current")
 
   return {
     r0: callResult[0],
