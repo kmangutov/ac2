4a31dfd44f2a333e9e186e3d34c33f61895af20b
Kirill Mangutov <kmangutov@gmail.com>
2018-09-12 20:33:18 -0700

    simple persistance

--- frontend/js/dashfront.js
+++ frontend/js/dashfront.js
@@ -9,4 +9,6 @@ if (Lockr.get('updated') === 'true') {
   $('#new-booking').show()
   $('#origin').text(Lockr.get('origin'))
   $('#destination').text(Lockr.get('destination'))
+
+  Lockr.set('updated', 'false')
 }
