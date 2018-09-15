76bd9215c30e448ec8e63ab05ed30b3d18ce1fab
Kirill Mangutov <kmangutov@gmail.com>
2018-09-15 19:05:40 -0400

    take out alert

--- frontend/js/contracts.js
+++ frontend/js/contracts.js
@@ -2,8 +2,6 @@
 if (Lockr.get('updated') === 'true') {
   $('.origin').text(Lockr.get('origin'))
   $('.destination').text(Lockr.get('destination'))
-
-  alert(Lockr.get('origin'))
 } else {
   $('.origin').text('Austin, USA')
   $('.destination').text('Beijing, China')
